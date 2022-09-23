8cb7b7d37b75fb5977fd4dcca868cc11af33d003
sunjiafu <84288682@qq.com>
2022-09-23 17:37:52 +0800

    update bitcoin

---
+++ app/Http/Controllers/pay/BitcoinContorller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\pay;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BitcoinContorller extends Controller
+{
+    public function notifyUrl(Request $request){
+
+
+        $raw_post_data = file_get_contents('php://input');
+        $log = fopen("bit.log",'ab');
+        $date = date('m/d/Y h:i:s a');
+      
+        if(!$raw_post_data){
+
+        
+
+            return 'fail';
+ 
+        };
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Livewire\Myorder;
 use App\Http\Livewire\Order;
 use App\Http\Livewire\Pay;
-use App\http\Controllers\pay\BtcController;
+use App\http\Controllers\pay\BitcoinContorller;
 use App\Http\Livewire\Servicelist;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +23,7 @@ Route::get('/', function () {
 })->name('index');
 
 Route::get('service', Servicelist::class);
-Route::post('btcpay/notify_url',[BtcController::class,'notifyUrl']);
+Route::post('btcpay/notify_url',[BitcoinContorller::class,'notifyUrl']);
 
 Route::middleware([
     'auth:sanctum',
