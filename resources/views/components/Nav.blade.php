690fdfa95029e5c24b8341c35e7c98de4c8e83fd
sunjiafu <84288682@qq.com>
2022-09-26 11:14:36 +0800

    add nav-link component

--- resources/views/components/Nav.blade.php
+++ resources/views/components/Nav.blade.php
@@ -81,10 +81,15 @@
     <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
       <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a href="{{ route('index') }}" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="index">首页</a>
+        <x-nav-link href="{{ route('index') }}" :active="request()->routeIs('index')">
+        首页
+        </x-nav-link>
+       
         </li>
         <li>
-          <a href="{{ route('service') }}" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">服务内容</a>
+        <x-nav-link href="{{ route('service') }}" :active="request()->routeIs('service')">
+        服务内容
+        </x-nav-link>
         </li>
 
       </ul>
