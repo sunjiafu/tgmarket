38f6d37db67fbf32a43911b92528239d414ac74d
sunjiafu <84288682@qq.com>
2022-09-26 11:23:36 +0800

    filament update

--- config/filament.php
+++ config/filament.php
@@ -37,7 +37,7 @@ return [
     |
     */
 
-    'core_path' => env('FILAMENT_CORE_PATH', 'filament'),
+    'core_path' => env('FILAMENT_CORE_PATH', 'vendor'),
 
     /*
     |--------------------------------------------------------------------------
