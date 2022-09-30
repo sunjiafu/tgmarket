1a14033d35e24d0ec288f6218d6f541d46994121
sunjiafu <84288682@qq.com>
2022-09-30 23:07:38 +0800

    updat post.balade

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -3,48 +3,54 @@
 @section('meta_keywords',$posts->auther)
 
 <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
-  <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-      <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-          <header class="mb-4 lg:mb-6 not-format">
-         
-              <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{$posts->title}}</h1>
-          </header> 
-          <style>
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header class="mb-4 lg:mb-6 not-format">
 
-         p{
+                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{$posts->title}}</h1>
+            </header>
+            <style>
+                p {
 
-            text-indent: 2rem;
-         }
+                    text-indent: 2rem;
+                }
 
-          ol{
+                ol {
 
-             list-style-type: disc;
-             list-style-position: inside;
+                    list-style-type: disc;
+                    list-style-position: inside;
 
-          }
+                }
 
-     .content a{
+                ul {
 
-     text-decoration-line: underline;
-     color: blue;
-          }
-        .content img{
+                    list-style-type: disc;
+                    list-style-position: inside;
 
-            max-width: 100%;
-            margin-left: auto;
-            margin-right: auto;
-            height: auto;
-            border-radius: 8px;
-        }
-          </style>
-   
-<div class="content py-6  space-y-1 text-gray-500 leading-loose  ">
-{!!$posts->content!!}
-</div>
-      </article>
-      @endforeach
-  </div>
+                }
+
+                .content a {
+
+                    text-decoration-line: underline;
+                    color: blue;
+                }
+
+                .content img {
+
+                    max-width: 100%;
+                    margin-left: auto;
+                    margin-right: auto;
+                    height: auto;
+                    border-radius: 8px;
+                }
+            </style>
+
+            <div class="content py-6  space-y-1 text-gray-500 leading-loose  ">
+                {!!$posts->content!!}
+            </div>
+        </article>
+        @endforeach
+    </div>
 </main>
 
 
-<img class=" mx-auto  max-w-lg h-auto rounded-lg"
