cc4f0b3da894902c0d3cec00a05458eeeed2bae1
sunjiafu <84288682@qq.com>
2022-10-01 11:30:47 +0800

    post update

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -1,15 +1,7 @@
 @foreach ($post as $posts)
 @section('pagetitle',$posts->title)
 @section('meta_keywords',$posts->auther)
-
-<main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
-    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-            <header class="mb-4 lg:mb-6 not-format">
-
-                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{$posts->title}}</h1>
-            </header>
-            <style>
+<style>
                 p {
 
                     text-indent: 2rem;
@@ -37,13 +29,49 @@
 
                 .content img {
 
+
                     max-width: 100%;
                     margin-left: auto;
                     margin-right: auto;
                     height: auto;
                     border-radius: 8px;
                 }
+
+                figure {
+
+                    max-width: 32rem
+                        /* 512px */
+                    ;
+
+
+                }
+
+                figcaption {
+
+                    margin-top: 0.5rem
+                        /* 8px */
+                    ;
+                    font-size: 0.875rem
+                        /* 14px */
+                    ;
+                    line-height: 1.25rem
+                        /* 20px */
+                    ;
+                    text-align: center;
+                    color: rgb(107 114 128 / var(--tw-text-opacity));
+
+
+                }
             </style>
+
+<main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header class="mb-4 lg:mb-6 not-format">
+
+                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{{$posts->title}}</h1>
+            </header>
+            
 
             <div class="content py-6  space-y-1 text-gray-500 leading-loose  ">
                 {!!$posts->content!!}
@@ -51,6 +79,8 @@
         </article>
         @endforeach
     </div>
+
 </main>
 
 
+<x-footer />
